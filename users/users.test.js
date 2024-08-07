3d906be16684128805c20c03b3dedfeb8ae429ae
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-07 15:26:51 +0100

    task 5

--- users/users.test.js
+++ users/users.test.js
@@ -5,8 +5,9 @@ import app from "../app.js";
 
 test("GET /api/health works", async () => {
     const response = await request(app).get("/api/health");
-  console.log(response.body)
-
+    expect(response.body).toEqual (
+ { success: true, payload: "API is running correctly" }
+    )
 })
 
 
@@ -19,3 +20,14 @@ test("GET /api/health works", async () => {
 //    send a GET request to the /api/health endpoint
 //    await the overall expression and assign it to a `response` constant
 //    console.log `response.body` for now to at least see a result
+
+
+// - Assert that the response body is an object with the structure below:
+//   - `{ success: true, payload: "API is running correctly" }`
+//   - Why: This is what the response body should always be for the `GET /api/health` endpoint
+//   - Vitest offers matchers that allow you check if an object has a particular structure. Have a look at the documentation and examples.
+// - Assert that the response status code is 200
+//   - Why: HTTP status code 200 indicates the request was successful and this is what the response status code should always be for the `GET /api/health` endpoint
+// - Assert that the Content-Type response header contains `application/json`.
+//   - Why: The `GET /api/health` endpoint should always respond with JSON and also let the client know that the response body contains JSON.
+
